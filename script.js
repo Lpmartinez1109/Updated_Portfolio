c20a561acc935286ff5812b14af63210136d37c9
LPMartinez <lpmartinez1109@gmail.com>
2020-05-08 16:15:04 -0500

    sidenav in works

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function openNav(){
-	document.getElementById("sidebar").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
+	document.getElementById("sideNav").style.width = "250px";
 console.log("click works");
 
 }
 function closeNav(){
-	document.getElementById("sidebar").style.width = "0";
-	document.getElementById("main").style.marginLeft = "0";
+	document.getElementById("sideNav").style.width = "0";
 }
 // $(window).on("load", function(){
 // 	$(".loader-wrapper").fadeOut("slow");
